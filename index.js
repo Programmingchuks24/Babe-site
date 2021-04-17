43cc9d5dfec3c32414b0210b2c376dde1deb8336
Chuks <59261316+Programmingchuks24@users.noreply.github.com>
2021-04-17 10:07:29 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const start_program = () => {
+
+	const unnamed_function = () => {
+
+		//function to be created at later date...
+	}
+
+	//code goes here
+	
+	//testing code
+	
+	console.log("Javascript working...");
+
+	console.log("Waiting for code...");
+}
+
+start_program();
